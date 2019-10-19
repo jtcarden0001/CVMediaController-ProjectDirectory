af99af1cafe8e01ff9133740fb55a1867819563c
Zatom01 <anil_gurung15@yahoo.com>
2019-10-19 03:45:51 -0500

    added elif statements using numpy logic

--- Computer Vision Module/face_detect.py
+++ Computer Vision Module/face_detect.py
@@ -1,14 +1,14 @@
 import cv2                          # importing openCV module
 import matplotlib.pyplot as plt     # importing matplotlib
+import numpy as np                  #importing numpy
 
-#face_count = 0
-
+                                                                            #letting the program know how each object looks
 face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')   #loading trained classifier for face detection
 eye_cascade=cv2.CascadeClassifier('haarcascade_eye.xml')                    #loading trained classifier for eye
-play_cascade=cv2.CascadeClassifier('fist_sandeep_github.xml')                      #loading trained classifier for closed fist
+play_cascade=cv2.CascadeClassifier('fist_sandeep_github.xml')               #loading trained classifier for closed fist
 pause_cascade=cv2.CascadeClassifier('cascade_pause.xml')                    #loading trained classifier for open palm
-vol_up_cascade=cv2.CascadeClassifier('v2_thumbs_up_Cascade_trainer.xml')
-vol_down_cascade=cv2.CascadeClassifier('v9_thumbsdown.xml')
+vol_up_cascade=cv2.CascadeClassifier('v3_thumbs_up_Cascade_trainer.xml')
+vol_down_cascade=cv2.CascadeClassifier('v10_thumbsdown.xml')
 right_sideways=cv2.CascadeClassifier('right sideways_sandeep_github.xml')
 
 #reads from the web cam
@@ -21,7 +21,8 @@ cap.set(3,1028)             # sets the output window resolution to 1028. code fo
 cap.set(4,720)              # sets the output window resolution to 720. code for the  width is 4
 
 
-while True:                  #While loop to capture the frame continuosly
+while True:    #While loop to capture the frame continuosly
+
     ret, frame= cap.read()   # defining variables 'ret' and 'frame'. With 'cap' instance, calling method read()
                              #read() will return True if frame is available and save it to variable 'frame'
                              #'ret' variable will save True or False
@@ -35,69 +36,80 @@ while True:                  #While loop to capture the frame continuosly
     #scaleFactor= Parameter specifying how much the image size is reduced at each image scale
     #minNeighbors= Parameters specifying how many neighbors each candidate rectangle should have to retain it
     #changing scaleFactor and minNeighbors to different values changes the sensitivity of the detection.
-    faces= face_cascade.detectMultiScale(gray,1.3,5)    #detecting faces on 'gray' and saving into variable 'faces'
-    play=play_cascade.detectMultiScale(gray,1.1,5)      #detecting closed fist and saving into varaible 'play'
-    pause=pause_cascade.detectMultiScale(gray,1.1,4)    #detecting open palm and saving into variable 'pause'
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) # detecting faces in each frame and saving into variable=faces
+    play = play_cascade.detectMultiScale(gray, 1.1, 5)  # detecting play in frame and  saving into variable=play
+    pause=pause_cascade.detectMultiScale(gray,1.1,4)
     vol_up=vol_up_cascade.detectMultiScale(gray,1.3,5)
     vol_down=vol_down_cascade.detectMultiScale(gray,1.1,1)
-    #unknown=unknown_cascade.detectMultiScale(gray,1.3,5)
     forward=right_sideways.detectMultiScale(gray,1.1,4)
 
 
-    for(x, y, w, h) in faces:                        #iterate over faces object. Parameters:x,y,width and height of the rectangle
 
-        #cv2.rectangle(image,starting point, ending point, color, thickness)
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 2) #draws rectangle around the face. starts from x and y to x+w and y+h
-        #face_count += 1
-        #print(face_count)
-        roi_gray =gray[y:y+h, x:x+w]    #setting a boundry around face.Starts from y to y+h...... Will be used for eye detection.
-        roi_color = frame[y:y+h, x:x+w]
-        eyes=eye_cascade.detectMultiScale(roi_gray)#detecting eye inside the facial region
 
-        for (ex,ey,ew,eh) in eyes:  #for loop inside faces to make sure eyes are never detected outside of the face
-            font = cv2.FONT_HERSHEY_TRIPLEX #setting the font
+    if (np.sum(faces)>0):                   #if x+y+w+h>0, draw a rectangle. np.sum() basically sums up the numpy array
+        print('face detected')
+        for (x, y, w, h) in faces:          # iterate over faces object. Parameters:x,y,width and height of the rectangle
 
-        # cv2.putText(image where text will be,text,cordinates of text, font, fontScale, color, thickness, line type )
-            text=cv2.putText(frame, 'face', (1135, 100), font, 1.5, (0, 0, 255), 2, cv2.LINE_AA)
+            # cv2.rectangle(image,starting point, ending point, color, thickness)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255),2)  # draws rectangle around the face. starts from x and y to x+w and y+h
+            roi_gray = gray[y:y + h,x:x + w]  # setting a boundry around face.Starts from y to y+h...... Will be used for eye detection.
+            roi_color = frame[y:y + h, x:x + w]
+            eyes = eye_cascade.detectMultiScale(roi_gray)  # detecting eye inside the facial region
+
+            for (ex, ey, ew, eh) in eyes:  # for loop inside faces to make sure eyes are never detected outside of the face
+                font = cv2.FONT_HERSHEY_TRIPLEX  # setting the font
+
+                # cv2.putText(image where text will be,text,cordinates of text, font, fontScale, color, thickness, line type )
+                text = cv2.putText(frame, 'face', (1135, 100), font, 1.5, (0, 0, 255), 2, cv2.LINE_AA)
+                plt.imshow(text)
+
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0),2)  # drawing rectangle around the eyes
+
+    elif (np.sum(play)> 0):
+        print('play detected')
+        for (px, py, pw, ph) in play:
+
+            cv2.rectangle(frame, (px, py), (px + pw, py + ph), (0, 255, 0), 2)
+            font = cv2.FONT_HERSHEY_TRIPLEX
+            text = cv2.putText(frame, '    PLAY    ', (-80, 150), font, 1.5, (0, 255, 0), 2, cv2.LINE_4)
             plt.imshow(text)
 
-            cv2.rectangle(roi_color,(ex,ey), (ex+ew, ey+eh), (0,255,0),2) #drawing rectangle around the eyes
+    elif (np.sum(pause)> 0):
+        print('pause detected')
+        for (psx, psy, psw, psh) in pause:
+            cv2.rectangle(frame, (psx, psy), (psx + psw, psy + psh), (18, 255, 255), 2)
+            font = cv2.FONT_HERSHEY_TRIPLEX
+            text = cv2.putText(frame, '     PAUSE    ', (-110, 250), font, 1.5, (18, 255, 255), 2, cv2.LINE_4)
+            plt.imshow(text)
 
+    elif (np.sum(vol_up)> 0):
+        print('volume up detected')
+        for (vux, vuy, vuw, vuh) in vol_up:
+            cv2.rectangle(frame, (vux, vuy), (vux + vuw, vuy + vuh), (18, 255, 255), 2)
+            font = cv2.FONT_HERSHEY_TRIPLEX
+            text = cv2.putText(frame, '     Volume up   ', (-110, 150), font, 1.5, (18, 255, 255), 2, cv2.LINE_4)
+            plt.imshow(text)
 
-    for ( px,py,pw,ph) in play:
+    elif (np.sum(vol_down)> 0):
+        print('volume down detected')
+        for (vdx, vdy, vdw, vdh) in vol_down:
+            cv2.rectangle(frame, (vdx, vdy), (vdx + vdw, vdy + vdh), (255, 255, 255), 2)
+            font = cv2.FONT_HERSHEY_TRIPLEX
+            text = cv2.putText(frame, '     Volume down   ', (-110, 300), font, 1.5, (255, 255, 255), 2, cv2.LINE_4)
+            plt.imshow(text)
 
-        cv2.rectangle(frame,(px,py),(px+pw, py+ph),(0,255,0),2)
-        font = cv2.FONT_HERSHEY_TRIPLEX
-        text = cv2.putText(frame, '    PLAY    ', (-80, 150), font, 1.5, (0,255,0), 2, cv2.LINE_4)
-        plt.imshow(text)
+    elif (np.sum(forward)> 0):
+        print('forward detected')
+        for (bx, by, bw, bh) in forward:
+            cv2.rectangle(frame, (bx, by), (bx + bw, by + bh), (255, 153, 255), 2)
+            font = cv2.FONT_HERSHEY_TRIPLEX
+            text = cv2.putText(frame, '     backward  ', (-120, 200), font, 1.5, (255, 153, 255), 2, cv2.LINE_4)
+            plt.imshow(text)
 
-    for ( psx, psy,psw,psh) in pause:
-        cv2.rectangle(frame, (psx,psy), (psx+psw, psy+psh),(18, 255, 255),2)
-        font = cv2.FONT_HERSHEY_TRIPLEX
-        text = cv2.putText(frame, '     PAUSE    ', (-110, 250), font, 1.5, (18, 255, 255), 2, cv2.LINE_4)
-        plt.imshow(text)
+    else:
+        print("nothing detected")
 
-    for (vux,vuy,vuw,vuh) in vol_up:
-        cv2.rectangle(frame, (vux, vuy), (vux + vuw, vuy + vuh), (18, 255, 255), 2)
-        font = cv2.FONT_HERSHEY_TRIPLEX
-        text = cv2.putText(frame, '     Volume up   ', (-110, 150), font, 1.5, (18, 255, 255), 2, cv2.LINE_4)
-        plt.imshow(text)
-
-
-    for (vdx,vdy,vdw,vdh) in vol_down:
-        cv2.rectangle(frame, (vdx, vdy), (vdx + vdw, vdy + vdh), (255, 255, 255), 2)
-        font = cv2.FONT_HERSHEY_TRIPLEX
-        text = cv2.putText(frame, '     Volume down   ', (-110, 300), font, 1.5, (255, 255, 255), 2, cv2.LINE_4)
-        plt.imshow(text)
-
-
-
-
-    for (bx,by,bw,bh) in forward:
-        cv2.rectangle(frame, (bx, by), (bx + bw, by + bh), (255, 153, 255), 2)
-        font = cv2.FONT_HERSHEY_TRIPLEX
-        text = cv2.putText(frame, '     backward  ', (-120, 200), font, 1.5, (255, 153, 255), 2, cv2.LINE_4)
-        plt.imshow(text)
 
     cv2.imshow('video',frame)           # to show the frame captured by the camera.
                                         #imshow(any name for output window, variable 'frame')
@@ -109,3 +121,4 @@ while True:                  #While loop to capture the frame continuosly
 
 cap.release()                       #releasing capture variable 'cap' and all the resources
 cv2.destroyAllWindows()             #destroy the output window
+
